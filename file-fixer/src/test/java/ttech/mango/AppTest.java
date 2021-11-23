584ecabb6f7935e667128be301fb40ac8b7a64c9
Melika O'Garro <m.ogarro@icloud.com>
2021-11-23 17:06:25 -0400

    Co-authored-by: DGuv <DGuv@users.noreply.github.com> Co-authored-by: JonG-exe <JonG-exe@users.noreply.github.com> Co-authored-by: AmieTorin <AmieTorin@users.noreply.github.com>

---
+++ file-fixer/src/test/java/ttech/mango/AppTest.java
@@ -0,0 +1,76 @@
+package ttech.mango;
+
+//import static org.junit.Assert.assertNotNull;
+//import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+// import java.io.File;
+
+// import java.util.ArrayList;
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Test the five scenarios
+     */
+    @Test
+
+
+    //Scenario 1 : Convention 1 only
+
+    public void allValidSubmissions()
+    {
+        //final String currentDirectory = System.getProperty("user.dir");
+        //File renamedAssigns = new File (currentDirectory + File.separator + "filesToRename" + File.separator + "Renamed Assignments");
+        //Directory renamedAssignsDir = new Directory(renamedAssigns.getName(), renamedAssigns.getParent());
+        //File noSubmissionList = new File (FileFixer.currentDirectory + File.separator + "filesToRename" + File.separator +"DID NOT SUBMIT.txt");
+      
+    }
+
+    //Scenario 2
+
+    public void allSubmissionsAreMissing()
+    {
+        // input: sample3 case_ii.zip
+
+
+    }
+
+
+
+    //Scenario 3 
+    public void validAndInvalidSubmssions()
+    {
+        // input: sample3 case_iii.zip
+
+    }
+
+
+
+    //Scenario 4
+    public void validAndMissingSubmissions()
+    {
+         // input: sample3 case_iv.zip
+
+    }
+
+    //Scenario 5
+    public void allInvalidSubmissions()
+    {
+         // input: sample5 case_v.zip
+
+    }
+
+
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    
+
+}
