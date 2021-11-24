60791ba91e8133969e4066a02aa0f2c637353f32
JonG-exe <82302223+JonG-exe@users.noreply.github.com>
2021-11-24 19:23:17 -0400

    Update SubmissionsTest.java
    Removed repetitive code from each method to one abstract class (SetUpTest).

--- file-fixer/src/test/java/ttech/mango/SubmissionsTest.java
+++ file-fixer/src/test/java/ttech/mango/SubmissionsTest.java
@@ -2,19 +2,15 @@ package ttech.mango;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import java.util.ArrayList;
 
 public class SubmissionsTest extends SetUpTest{
+    
+    // attributes such as "numFilesRenamed" obtains its values from the parent Class "SetUpTest"
 
     @Test
     public void AllValidSubmissionsTest(){
         // Scenario 1 - 49 names to rename, all valid
-
-        ToRenameCollection root = new ToRenameCollection(currentDirectory, folderName, renamedFilesFolderName, noSubmissionListFileName);
-        root.initFilesToRenameDir(); //unzipping
-        ArrayList <Student> students = root.readCSV();
         
-        int numFilesRenamed = root.processPDFs(); // the actual for the 'assert' tests -> the no. of files renamed is returned
         int expected = 49;  // 49 students are expected to be renamed after submissions
         root.createDidNotSubmit(students);
 
@@ -25,65 +21,40 @@ public class SubmissionsTest extends SetUpTest{
     public void NoPDFsToRenameTest(){
         // Scenario 2 - 0 PDFs to renamed, 49 missing submissions listed
 
-        ToRenameCollection root = new ToRenameCollection(currentDirectory, folderName, renamedFilesFolderName, noSubmissionListFileName);
-        root.initFilesToRenameDir(); //unzipping
-        ArrayList <Student> students = root.readCSV();
-        
-        int numFilesRenamed = root.processPDFs(); // the actual for the 'assert' tests -> the no. of files renamed is returned
         int expected = 0; // The number of students that is expected to be renamed from 0 submissions
         root.createDidNotSubmit(students);
 
-        assertEquals(expected, numFilesRenamed);
-
-        if(expected == numFilesRenamed) 
-            printDidNotSubmit();; // 49 students are expected to be listed for missing submissions
+        assertEquals(expected, numFilesRenamed); // 49 students are expected to be listed for missing submissions
     }
 
     @Test
     public void SomeInvalidFlaggedSubmissionsTest(){
         // Scenario 3 - 49 names to rename, 30 valid and 19 invalid
 
-        ToRenameCollection root = new ToRenameCollection(currentDirectory, folderName, renamedFilesFolderName, noSubmissionListFileName);
-        root.initFilesToRenameDir(); //unzipping
-        ArrayList <Student> students = root.readCSV();
-        int numFilesRenamed = root.processPDFs(); // the actual for the 'assert' tests -> the no. of files renamed is returned
-
         int expected = 30; // 30 students are expected to be renamed after submissions
-        root.createDidNotSubmit(students);
+        root.createDidNotSubmit(students); // 19 students are flagged for invalid submissions in the "DID NOT SUBMIT.txt" file
         
-        assertEquals(expected, numFilesRenamed);
+        assertEquals(expected, numFilesRenamed); 
 
         if(expected == numFilesRenamed) 
-            printDidNotSubmit();; // 19 students are expected to be listed for flagged submissions
+            printDidNotSubmit(); // 19 Students are expected to be listed in the debug console
     }
 
     @Test
     public void SomeInvalidSubmissionsListTest(){
         // Scenario 4
 
-        ToRenameCollection root = new ToRenameCollection(currentDirectory, folderName, renamedFilesFolderName, noSubmissionListFileName);
-        root.initFilesToRenameDir(); //unzipping
-        ArrayList <Student> students = root.readCSV();
-        int numFilesRenamed = root.processPDFs(); // the actual for the 'assert' tests -> the no. of files renamed is returned
-
         int expected = 30; // 30 students are expected to be renamed after submissions
         root.createDidNotSubmit(students);
         
         assertEquals(expected, numFilesRenamed);
 
-        if(expected == numFilesRenamed) 
-            printDidNotSubmit(); // 19 Students are expected to be listed
     }
 
     @Test
     public void AllInvalidFlaggedSubmissionsTest(){
         // Scenario 5
 
-        ToRenameCollection root = new ToRenameCollection(currentDirectory, folderName, renamedFilesFolderName, noSubmissionListFileName);
-        root.initFilesToRenameDir(); //unzipping
-        ArrayList <Student> students = root.readCSV();
-        
-        int numFilesRenamed = root.processPDFs(); // the actual for the 'assert' tests -> the no. of files renamed is returned
         int expected = 0;  // 49 submissions are expected to be invalid, therefore ZERO are renamed
         root.createDidNotSubmit(students);
 
