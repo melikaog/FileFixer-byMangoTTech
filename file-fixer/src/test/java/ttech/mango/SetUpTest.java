a9a9d41c93f76e76a1a9b7331caf79729ab4c5cb
JonG-exe <82302223+JonG-exe@users.noreply.github.com>
2021-11-24 19:24:38 -0400

    Update SetUpTest.java
    Added new attributes so that classes that inherit from this class do not need to have repeating code.

--- file-fixer/src/test/java/ttech/mango/SetUpTest.java
+++ file-fixer/src/test/java/ttech/mango/SetUpTest.java
@@ -3,6 +3,7 @@ package ttech.mango;
 import java.io.File;
 import org.junit.jupiter.api.BeforeAll;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public abstract class SetUpTest{
     public static File renamedAssigns;
@@ -10,7 +11,12 @@ public abstract class SetUpTest{
     public static String folderName = "filesToRename";
     public static String renamedFilesFolderName = "Renamed Assignments";
     public static String noSubmissionListFileName = "DID NOT SUBMIT.txt";
+    public static ArrayList <Student> students;
+    public static ToRenameCollection root;
+    public static int numFilesRenamed;
 
+    // this method sets up the environment variables necessary for the SubmissionsTest to run
+    // this method will allows run before any of the other methods in SubmissionsTest can run
     @BeforeAll
     public static void runSetUpTest(){
         
@@ -18,6 +24,12 @@ public abstract class SetUpTest{
         renamedAssigns = new File(currentDirectory + File.separator + folderName + File.separator + renamedFilesFolderName);
         renamedAssigns.delete();
         renamedAssigns.mkdir();
+
+        root = new ToRenameCollection(currentDirectory, folderName, renamedFilesFolderName, noSubmissionListFileName);
+        root.initFilesToRenameDir(); //unzipping
+        students = root.readCSV();
+        
+        numFilesRenamed = root.processPDFs(); // the actual for the 'assert' tests -> the no. of files renamed is returned
     }     
 
     public static void printDidNotSubmit(){
